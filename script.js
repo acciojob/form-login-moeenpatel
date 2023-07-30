36808c92b37428d03721a16078f912ee8424821c
moeenpatel <123477545+moeenpatel@users.noreply.github.com>
2023-07-30 21:38:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
-    //Write your code here
+    const fname = document.querySelector('input[name="fname"]').value;
+	const lname = document.querySelector('input[name="lname"]').value;
+	alert(`${fname} ${lname}`);
 
 }
